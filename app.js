7f2f378e82b98582c476faf0ba5a28caa22fbd95
Rupesh Dabbir <rdabbir@cisco.com>
2017-03-31 23:28:40 -0700

    dev: new: Postgres setup + data retrieval
    - Added logic for PostGIS and data retrieval.

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var admin  = require('./routes/admin/index')
-var users = require('./routes/users/index');
+// var admin  = require('./routes/admin/index')
+// var users = require('./routes/users/index');
+var index = require('./routes/index');
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/users', users);
-app.use('/admin', admin);
+app.use('/',index);
+// app.use('/users', users);
+// app.use('/admin', admin);
 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+//
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 app.listen(3000);
 
